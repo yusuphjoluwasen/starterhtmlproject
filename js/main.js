9500f982a795781de1e23c80f923bbc9e060e23c
yusuphjoluwasen <yusuphjoluwaseun@gmail.com>
2020-04-22 14:31:20 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+export const Players = [
+    {
+        name:"Christiano Ronaldo",
+        age:19,
+        salary:"N19,000",
+        hobbies:"Reading",
+        image:"https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d2388f14c687b00085c0f91%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D1559%26cropY1%3D130%26cropY2%3D1690"
+    },
+    {
+        name:"Lionel Messi",
+        age:23,
+        salary:"N55,000",
+        hobbies:"Skating"
+    },
+    {
+        name:"Didier Drogba",
+        age:25,
+        salary:"N720,000",
+        hobbies:"Fashion Design"
+    },
+    {
+        name:"Mohammed Salah",
+        age:34,
+        salary:"N190,000",
+        hobbies:"Photography"
+    },
+    {
+        name:"",
+        age:19,
+        salary:"N19,000",
+        hobbies:"Practical Jokes"
+    },
+    {
+        name:"Jan Oblak",
+        age:21,
+        salary:"N19,000",
+        hobbies:"Video editing"
+    },
+    {
+        name:"Manuel Neuer",
+        age:35,
+        salary:"N19,000",
+        hobbies:"Video editing"
+    },
+    {
+        name:"Hugo Lloris",
+        age:42,
+        salary:"N39,000",
+        hobbies:"Lace Making"
+    },
+    {
+        name:"Roberto Firmino",
+        age:12,
+        salary:"N89,000",
+        hobbies:"Playing Fifa"
+    },
+    {
+        name:"Eden Hazard",
+        age:55,
+        salary:"N95,000",
+        hobbies:"Graphic Designing"
+    }
+]
+
+
+
